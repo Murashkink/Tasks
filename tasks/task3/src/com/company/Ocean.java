fea5b1e1607fc6a3bc79f8d19e5e1db3e55de8ab
murashkink <murashkin.k.v@gmail.com>
2022-11-12 13:51:39 +0200

    Задачи

---
+++ tasks/task3/src/com/company/Ocean.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Ocean {
+    private String name;
+
+    public Ocean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- tasks/task6/src/com/company/Task6.java
+++ tasks/task6/src/com/company/Task6.java
@@ -1,18 +1,30 @@
 package com.company;
 
+import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Task6 {
+
     public static void main(String[] args) {
-        System.out.print("Введите строку: ");
-        Scanner line = new Scanner(System.in);
-        String str = line.nextLine();
-        char[] chars = str.toCharArray();
-        int sum = 0;
-        for (int i = 0; i < chars.length; i++) {
-            if ((byte) chars[i] >= 0x30 && (byte) chars[i] <= 0x39)
-                sum += (byte) chars[i] - 0x30;
+        try {
+            System.out.print("Введите строку: ");
+            Scanner line = new Scanner(System.in);
+            String str = line.nextLine();
+            char[] chars = str.toCharArray();
+            int sum = 0;
+            for (int i = 0; i < chars.length; i++) {
+                if ((byte) chars[i] >= 0x30 && (byte) chars[i] <= 0x39)
+                    sum += (byte) chars[i] - 0x30;
+
+            }
+            if (sum == 0) {
+                System.err.println("В строке нет чисел");
+            }
+            System.out.println("Сумма всех целых чисел(цифр) в строке равна " + sum);
+
+        } finally {
+            System.out.println("Программа завершена успешно. ");
         }
-        System.out.println("Сумма всех целых чисел(цифр) в строке равна " + sum);
     }
 }
